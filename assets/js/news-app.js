aa06c4329331a0f3b599fe801319efecd9c129a5
Harsh0166 <2005harshkumar@gmail.com>
2024-09-20 23:04:08 +0530

    update

--- assets/js/news-app.js
+++ assets/js/news-app.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+ document.addEventListener("DOMContentLoaded", function() {
     const newsTexts = document.querySelectorAll(".news-text");
 
     newsTexts.forEach((newsText) => {
