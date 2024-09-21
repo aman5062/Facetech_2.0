ae71490068b63162a94b6ca5a48ce7d348065ad4
Shashank1197 <wwwladdu94@gmail.com>
2024-09-21 11:47:38 +0530

    profilepage

--- assets/php/news.php
+++ assets/php/news.php
@@ -15,43 +15,30 @@
         </nav>
     </header>
 
-    <!-- Main Content Section -->
-    <main class="news-content">
-        <div class="news-image">
-            <img src="../pictures/news-2.jpeg" alt="News Image">
+
+   <?php 
+   include_once("db.php");
+   $sql = "SELECT * FROM `news`";
+   $result = mysqli_query($conn,$sql);
+   if($result->num_rows>0){
+    while($row = $result-> fetch_assoc()){
+        echo "
+        <main class='news-content'>
+        <div class='news-image'>
+            <img src='".$row["Image"]."' alt='News Image'>
         </div>
-        <div class="news-text">
-            <h1>Breaking News: Major Event Happening Now</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse convallis, nisl non luctus vehicula, mauris magna sodales ligula, eu vehicula ligula eros vel turpis. Mauris vulputate urna sit amet justoconsectetur adipisicing elit. Sapiente, voluptas eveniet. Asperiores quisquam doloremque nihil odit ad voluptatibus officiis architecto facere dolor veniam, magnam dignissimos, et ea, cupiditate sequi aliquid! Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis amet reiciendis laudantium unde voluptatum ab, velit nesciunt quae suscipit quos et neque laborum quaerat ratione nulla quas commodi, nobis consectetur. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi consequuntur assumenda sint mollitia, aliquam eius sunt vitae odit numquam magnam sequi suscipit dolorum saepe consequatur fuga? Eaque est rerum iure? vehicula, ac cursus arcu iaculis.</p>
-        </div>
-    </main>
-    <main class="news-content">
-        <div class="news-image">
-            <img src="../pictures/news-3.webp" alt="News Image">
-        </div>
-        <div class="news-text">
-            <h1>Breaking News: Major Event Happening Now</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse convallis, nisl non luctus vehicula, mauris magna sodales ligula, eu vehicula ligula eros vel turpis. Mauris vulputate urna sit amet justo vehicula, ac cursus arcu iaculis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, voluptas eveniet. Asperiores quisquam doloremque nihil odit ad voluptatibus officiis architecto facere dolor veniam, magnam dignissimos, et ea, cupiditate sequi aliquid! Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis amet reiciendis laudantium unde voluptatum ab, velit nesciunt quae suscipit quos et neque laborum quaerat ratione nulla quas commodi, nobis consectetur. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi consequuntur assumenda sint mollitia, aliquam eius sunt vitae odit numquam magnam sequi suscipit dolorum saepe consequatur fuga? Eaque est rerum iure? </p>
+        <div class='news-text'>
+            <h1>".$row["Title"]."</h1>
+            <p>".$row["Content"]."</p>
         </div>
     </main>
-    <main class="news-content">
-        <div class="news-image">
-            <img src="../pictures/news.jpg" alt="News Image">
-        </div>
-        <div class="news-text">
-            <h1>Breaking News: Major Event Happening Now</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse convallis, nisl non luctus vehicula, mauris magna sodales ligula, eu vehicula ligula eros vel turpis. Mauris vulputate urna sit amet justo consectetur adipisicing elit. Sapiente, voluptas eveniet. Asperiores quisquam doloremque nihil odit ad voluptatibus officiis architecto facere dolor veniam, magnam dignissimos, et ea, cupiditate sequi aliquid! Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis amet reiciendis laudantium unde voluptatum ab, velit nesciunt quae suscipit quos et neque laborum quaerat ratione nulla quas commodi, nobis consectetur. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi consequuntur assumenda sint mollitia, aliquam eius sunt vitae odit numquam magnam sequi suscipit dolorum saepe consequatur fuga? Eaque est rerum iure?vehicula, ac cursus arcu iaculis.</p>
-        </div>
-    </main>
-    <main class="news-content">
-        <div class="news-image">
-            <img src="../pictures/news-1.jpeg" alt="News Image">
-        </div>
-        <div class="news-text">
-            <h1>Breaking News: Major Event Happening Now</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse convallis, nisl non luctus vehicula, mauris magna sodaconsectetur adipisicing elit. Sapiente, voluptas eveniet. Asperiores quisquam doloremque nihil odit ad voluptatibus officiis architecto facere dolor veniam, magnam dignissimos, et ea, cupiditate sequi aliquid! Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis amet reiciendis laudantium unde voluptatum ab, velit nesciunt quae suscipit quos et neque laborum quaerat ratione nulla quas commodi, nobis consectetur. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi consequuntur assumenda sint mollitia, aliquam eius sunt vitae odit numquam magnam sequi suscipit dolorum saepe consequatur fuga? Eaque est rerum iure?les ligula, eu vehicula ligula eros vel turpis. Mauris vulputate urna sit amet justo vehicula, ac cursus arcu iaculis.</p>
-        </div>
-    </main>
+        ";
+    }
+   }else{
+    echo "No Data";
+   }
+    
+    ?>
 
     <!-- Footer Section -->
     <footer>
