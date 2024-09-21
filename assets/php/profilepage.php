9275c6ebc179daef3f12a8f910f4b3661057b9ce
nikhil-kumar490 <927nikhilkumar@gmail.com>
2024-09-22 00:12:17 +0530

    re

--- assets/php/profilepage.php
+++ assets/php/profilepage.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Facebook Profile</title>
     <link rel="stylesheet" href="../css/blog.css">
-    
 </head>
 <body>
     <header>
